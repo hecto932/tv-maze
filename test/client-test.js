e3c8d7e5f90d671a4e6e3242121b73c113f21632
Hector Flores <hecto932@gmail.com>
2017-02-02 01:25:46 -0600

    Unit Testing / Mocks / Stubs

--- test/client-test.js
+++ test/client-test.js
@@ -1,10 +1,48 @@
 var test = require('tape')
 var tvmaze = require('../')
+var Client = require('../lib/client')
+var nock = require('nock')
 
-test('should create a client', function(t) {
+var endpoint = 'http://api.tvmaze.test'
+
+test('should create a client', function (t) {
   t.ok(tvmaze.createClient, 'should exist')
   t.equals(typeof tvmaze.createClient, 'function', 'should be a function')
+  var client = tvmaze.createClient()
+  t.ok(client instanceof Client, 'should be instance of client')
   t.end()
 })
 
+test('should list shows', function (t) {
+  var client = tvmaze.createClient({ endpoint: endpoint })
 
+  t.equals(typeof client.shows, 'function', 'should be a function')
+
+  nock(endpoint)
+    .get('/shows')
+    .reply(200, [])
+
+  client.shows(function (err, shows) {
+    t.error(err, 'should be not an error')
+    t.ok(Array.isArray(shows), 'should be an array')
+    t.end()
+  })
+})
+
+test('should search shows', function (t) {
+  var client = tvmaze.createClient({ endpoint: endpoint })
+
+  t.equals(typeof client.search, 'function', 'should be a function')
+
+  nock(endpoint)
+    .get('/search/shows')
+    .query({ q: 'limitless' })
+    .reply(200, [{ name: 'Limitless' }])
+
+  client.search('limitless', function (err, shows) {
+    t.error(err, 'should be an error')
+    t.ok(Array.isArray(shows), 'should be an array')
+    t.equals(shows[0].name, 'Limitless', 'show retrieve a show name')
+    t.end()
+  })
+})
